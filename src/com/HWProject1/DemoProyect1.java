a6af110da60e4bdaf9840093acc9a1f0a14da58a
Rimsky <Rimsky@192.168.1.145>
2023-04-09 13:18:07 -0700

    commit add()method

--- src/com/HWProject1/DemoProyect1.java
+++ src/com/HWProject1/DemoProyect1.java
@@ -5,6 +5,13 @@ public class DemoProyect1 {
 	public static void main(String[] args) {
 		
 		System.out.println("New test project1");
+		
+		System.out.println("New test project1 stashes");
 	}
 
+	
+	public void add(int a,int b) {
+		
+		System.out.println(a+b);
+	}
 }
